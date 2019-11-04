6edb2ec2505793dfb1b92e47eacfe82cf1af4143
Jomar Mendoza <jmm21@Jomars-MacBook-Pro.local>
2019-11-04 15:36:55 -0800

    deleted extra files

--- js/app.js
+++ js/app.js
@@ -5,21 +5,21 @@ canvas.width = 900;
 canvas.height = 300;
 //
 const marioPic = new Image();
-marioPic.src = "img/marioSmall.png"
+marioPic.src = "img/marioSmall.png";
 
 const enemyPic = new Image();
-enemyPic.src = "img/enemies.png"
+enemyPic.src = "img/enemies.png";
 
 const groundPic = new Image();
-groundPic.src = "img/blocks.png"
+groundPic.src = "img/blocks.png";
 
 const castlePic = new Image();
-castlePic.src = "img/bg-1-1.png"
+castlePic.src = "img/bg-1-1.png";
 
 
 
 let gameLive = true;
-let worldNumber = 2;
+let worldNumber = 0;
 
 let player = {
   x: 10,
@@ -84,7 +84,6 @@ class Tube {
   }
 }
 
-
 let control = {
 
   left: false,
@@ -106,16 +105,12 @@ let control = {
   }
 }
 
-
-
 const keyControl = () => {
   if (control.up && player.jumping === false) {
     player.yVelo -= 30;
     player.jumping = true;
   }
   if (control.left) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // ctx.drawImage(marioPic,0,72,36,72,player.x, player.y, player.w, player.h);
     player.xVelo -= 0.5;
   }
   if (control.right) {
@@ -126,79 +121,70 @@ const keyControl = () => {
 const world1 = () => {
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  const small1 = new Tube(670, 225, 50, 50)
+  const small1 = new Tube(670, 225, 50, 50);
 
-  ctx.drawImage(castlePic,0,0,600,300,0,0,900,400)
+  ctx.drawImage(castlePic,0,0,600,300,0,0,900,400);
 
   ctx.drawImage(marioPic,0,144,36,72,player.x, player.y, player.w, player.h);
 
   ctx.drawImage(enemyPic,0,0,29,29,enemy1.x,enemy1.y,enemy1.w,enemy1.h);
-  ctx.fillStyle = 'brown';
-  // ctx.fillRect(small1.x,small1.y,small1.w,small1.h)
-
-  ctx.fillStyle = 'brown';
-  // ctx.fillRect(480, 180, 120, 5)
 
   if(collision(player, small1)){
-    if((player.x + player.w) < small1.x && (player.y + player.h) > small1.y){
-      player.x = small1.x - player.w
+    if(Math.floor(player.x + player.w) < Math.floor(small1.x + player.w) ){
+      player.x = small1.x - player.w;
     } else if(player.x < small1.x && player.y > small1.y){
-      player.x = small1.x - player.w
+      player.x = small1.x - player.w;
     } else if(player.y < small1.y){
       player.y = small1.y - player.h;
       if(player.yVelo > 0){
         player.jumping = false
       }
     }else if(player.x > small1.x && player.y > small1.y){
-      player.x = small1.x + small1.w
+      player.x = small1.x + small1.w;
     }
   }
 
 
   if(collision(player, enemy1)){
     gameLive = false;
+    $('canvas').remove();
+    $('body').append($("<h1>Game Over</h1>"));
+    $('body').css('background', 'red');
   }
 
   if(collision(player, highGround)){
     player.y = highGround.y - 60;
     player.yVelo *= 0.7;
     if(player.yVelo > 0){
-      player.jumping = false
+      player.jumping = false;
     }
   }
 
   enemy1.x += enemy1.speed
   if(enemy1.x <= 200){
     enemy1.x = 200;
-    enemy1.speed *= -1
+    enemy1.speed *= -1;
   } else if (enemy1.x >= 650){
     enemy1.x = 650;
-    enemy1.speed *= -1
+    enemy1.speed *= -1;
   }
 }
-
 const world2 = () => {
-  const front = new Tube(15 ,203, 50, 60)
-  const small2 = new Tube(203, 180, 50, 80)
-  const medium2 = new Tube(465, 180, 50, 88)
+  const front = new Tube(15 ,203, 50, 60);
+  const small2 = new Tube(203, 180, 50, 80);
+  const medium2 = new Tube(465, 180, 50, 88);
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(castlePic,600,0,600,300,0,0,900,400)
-
-
+  ctx.drawImage(castlePic,600,0,600,300,0,0,900,400);
   ctx.drawImage(marioPic,0,144,36,72,player.x, player.y, player.w, player.h);
-
   ctx.drawImage(enemyPic,0,0,29,29,enemy2.x,enemy2.y,enemy2.w,enemy2.h);
 
 
-  ctx.fillStyle = 'green'
-  // ctx.fillRect(front.x,front.y,front.w,front.h)
-
-  ctx.fillStyle = 'green'
-  // ctx.fillRect(medium2.x,medium2.y,medium2.w,medium2.h)
-
   if(collision(player, enemy2)){
     gameLive = false;
+    $('canvas').remove()
+    $('body').append($("<h1>Game Over</h1>"))
+    $('body').css('background', 'red')
   }
 
   if(collision(player, front)){
@@ -255,15 +241,14 @@ const world2 = () => {
     enemy2.speed *= -1
   }
 }
-
 const world3 = () => {
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   const high1 = new Ground (47, 180, 74, 5)
   const high2 = new Ground (119, 95, 193, 3)
-  const high3 = new Ground (383, 95, 95, 1)
-  const high4 = new Ground (600, 180, 50, 1)
+  const high3 = new Ground (383, 95, 95, 3)
+  const high4 = new Ground (600, 180, 50, 5)
   ctx.drawImage(castlePic,1200,0,600,300,0,0,900,400)
 
   ctx.drawImage(marioPic,0,144,36,72,player.x, player.y, player.w, player.h);
@@ -271,24 +256,22 @@ const world3 = () => {
 
   ctx.drawImage(enemyPic,0,0,29,29,enemy3.x,enemy3.y,enemy3.w,enemy3.h);
 
-  ctx.fillStyle = 'brown';
-  ctx.fillRect(world.x, world.y, world.w, world.h,)
-
-  ctx.fillStyle = 'brown';
+  ctx.fillStyle = 'blue';
   ctx.fillRect(high1.x, high1.y, high1.w, high1.h,)
-
-  ctx.fillStyle = 'brown';
+  ctx.fillStyle = 'blue';
   ctx.fillRect(high2.x, high2.y, high2.w, high2.h,)
-  ctx.fillStyle = 'brown';
+  ctx.fillStyle = 'blue';
   ctx.fillRect(high3.x, high3.y, high3.w, high3.h,)
-  ctx.fillStyle = 'brown';
+  ctx.fillStyle = 'blue';
   ctx.fillRect(high4.x, high4.y, high4.w, high4.h,)
 
-  if(collision(player, enemy2)){
-    if(player.y + player.h < enemy2.y){
-      console.log('hit')
-    }
+  if(collision(player, enemy3)){
+    gameLive = false;
+    $('canvas').remove();
+    $('body').append($("<h1>Game Over</h1>"));
+    $('body').css('background', 'red');
   }
+
 
   if(collision(player, high1)){
     player.y = high1.y - 60;
@@ -304,47 +287,53 @@ const world3 = () => {
       player.jumping = false
     }
   }
-
-  //
-  // if(isCollision(player, high4)){
-  //   player.yVelo *= 0.7
-  //   player.y = high4.y - 60
-  //   if(player.yVelo > 0){
-  //     player.jumping = false;
-  //   }
-  // }
-
   if(collision(player, high3)){
-    player.yVelo *= 0.7
-    player.y = high3.y - 60
+    player.y = high3.y - 60;
+    player.yVelo *= 0.7;
     if(player.yVelo > 0){
-      player.jumping = false;
+      player.jumping = false
+    }
+  }
+  if(collision(player, high4)){
+    player.y = high4.y - 60;
+    player.yVelo *= 0.7;
+    if(player.yVelo > 0){
+      player.jumping = false
     }
   }
 
 
 
-  enemy2.x += enemy2.speed
-  if(enemy2.x <= 200){
-    enemy2.x = 200;
-    enemy2.speed *= -1
-  } else if (enemy2.x >= 450){
-    enemy2.x = 450;
-    enemy2.speed *= -1
+
+  enemy3.x += enemy3.speed
+  if(enemy3.x <= 339){
+    enemy3.x = 339;
+    enemy3.speed *= -1
+  } else if (enemy3.x >= canvas.width){
+    enemy3.x = canvas.width;
+    enemy3.speed *= -1
   }
 
 }
-
 const world4 = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(castlePic,2000,0,600,300,0,0,900,400)
   ctx.drawImage(marioPic,0,144,36,72,player.x, player.y, player.w, player.h);
 }
 const world5 = () => {
+  const goal = new Ground(710,10, 10 ,600)
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(castlePic,2700,0,600,300,0,0,900,400)
   ctx.drawImage(marioPic,0,144,36,72,player.x, player.y, player.w, player.h);
 
+
+  if(collision(player, goal)){
+    gameLive = false;
+    $('canvas').remove()
+    $('body').append($("<h1>good game</h1>"))
+    $('body').css('background', 'green')
+
+  }
 }
 
 const drawImage = () => {
@@ -358,7 +347,6 @@ const drawImage = () => {
     world3()
   }
   else if (worldNumber === 3){
-    update()
     world4()
   }
   else if (worldNumber === 4) {
@@ -396,11 +384,9 @@ const loop = () => {
     gameLive = false;
   }
 
-
   update()
   keyControl()
   drawImage()
-
 
   if(gameLive){
     requestAnimationFrame(loop)
@@ -414,37 +400,10 @@ const collision = (rect1, rect2) =>{
   rect1.y + rect1.h > rect2.y;
 }
 
-// const isCollision = (rect1, rect2) => {
-//  if(rect1.y + rect1.h > rect2.y) {return false};
-//  return true;
-// }
-
-// const isCollision = (rect1, rect2) => {
-//   if(rect1.x > (rect2.x + rect2.w)){ return false}
-//   if((rect1.x + rect1.w) < rect2.x){return false}
-//   if(rect1.y > (rect2.y + rect2.h)){ return false}
-//   if((rect1.y + rect1.h) < rect2.y){return false}
-//   return true;
-// }
 
 const enemy1 = new Enemy(200, 240, 35, 35, 1.5)
 const enemy2 = new Enemy(250, 240, 35, 35, 1.5)
-const enemy3 = new Enemy(200, 240, 35, 35, 2)
+const enemy3 = new Enemy(339, 240, 35, 35, 2)
 document.addEventListener('keydown', control.keyListiner)
 document.addEventListener('keyup', control.keyListiner)
 loop()
-
-
-
-// if(collision(player, small1)){
-//   if(player.x + player.w < Math.floor(small1.x + player.w)){
-//      player.x = small1.x - player.w
-//   }else if(Math.floor(player.y + player.h) < Math.floor(small1.y + player.h)){
-//     player.y = small1.y - player.h;
-//     if(player.yVelo > 0){
-//       player.jumping = false
-//     }
-//   }else if(player.x > Math.floor(small1.x + small1.w) && player.y + player.w > small1.y){
-//     player.x = small1.x + small1.w
-//   }
-// }
